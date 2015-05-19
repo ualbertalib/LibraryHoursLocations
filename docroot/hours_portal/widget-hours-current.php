b8df0f90c539098278d72db40cdf08a7628df215
Jeremy Hennig <jeremy.hennig@ualberta.ca>
2015-05-19 13:22:33 -0400

    modify  content-type to javascript of php file

--- docroot/hours_portal/widget-hours-current.php
+++ docroot/hours_portal/widget-hours-current.php
@@ -2,7 +2,7 @@
 include('config_other.inc.php');
 $directory = dirname(dirname(__FILE__));
 require_once('functions.php');
-
+header('Content-Type: text/javascript; charset=UTF-8');
           
 
       $widget = displayLocationsStatusWidget(); 
